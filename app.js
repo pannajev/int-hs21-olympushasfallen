638bd360f29bb3f63fa829b6612972b3cb10c536
Armend Halili <halilarm@students.zhaw.ch>
2021-11-06 18:26:49 +0100

    agileLevel1

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var i = numbers[Math.floor(Math.random()*numbers.length)]
                 var data = await response.json();
                 var { name, year, imgPath } = data;
 
-                document.getElementById('name').textContent = name;
-                document.getElementById('year').textContent = year;
+                document.getElementById('name'+i).textContent = name;
+                document.getElementById('year'+i).textContent = year;
                 document.getElementById('i').innerHTML = i;
                 document.getElementById("demo").innerHTML = "Nice gefunden"
                 document.getElementById('img'+i).src = imgPath;
