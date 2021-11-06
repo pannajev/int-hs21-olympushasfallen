44d6ac4f5f857af9ea2493f2e41f596aff4df898
Jevin Pannarakunnel <pannajev@students.zhaw.ch>
2021-11-06 17:27:30 +0100

    Level 1

--- app.js
+++ app.js
@@ -50,25 +50,34 @@ async function getPresidents5() {
     }
 
 */
-var numbers = ["1","2","3","4","5","6"] 
-var i =  Math.floor(Math.random() * numbers.length);
 
 
 // Randomizer for Zahlen von 1 - 10
-//var i = 0;
-async function getPresidents() {
-    //i += 1;
-    var numbers = ["1","2","3","4","5","6"] 
-    var i =  Math.floor(Math.random() * numbers.length);
-    
-    
-    var api = "https://61867a63cd8530001765ab06.mockapi.io/olhafa/presidents/"+i;
-        var response = await fetch(api);
-        var data = await response.json();
-        var { name, year, imgPath } = data;
+var numbers = ["1","2","3","4","5","6","7","8",]
+var x = 0;
 
-        document.getElementById('name').textContent = name;
-        document.getElementById('img').src = imgPath;
-        document.getElementById('year').textContent = year;
-        document.getElementById('i').innerHTML = i;
+async function getPresidents() {
+var i = numbers[Math.floor(Math.random()*numbers.length)]
+    if (x<=5) {
+        if (i>0 && i<6) {
+            var api = "https://61867a63cd8530001765ab06.mockapi.io/olhafa/presidents/"+i;
+                var response = await fetch(api);
+                var data = await response.json();
+                var { name, year, imgPath } = data;
+
+                document.getElementById('name').textContent = name;
+                //document.getElementById('img').src = imgPath;
+                document.getElementById('year').textContent = year;
+                document.getElementById('i').innerHTML = i;
+                document.getElementById("demo").innerHTML = "Nice gefunden"  
+            x++; 
+        }
+        else {
+            document.getElementById('i').innerHTML = i;
+            document.getElementById("demo").innerHTML = "Das war leider kein richtiger Kletterpunkt"
+        }
+    }
+    else{
+        document.getElementById("demo").innerHTML = "Next Page"
+    }
 }
